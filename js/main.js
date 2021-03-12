e1218fa19884806badae8c33ecec16ae980158ed
NamanGoyal07 <naman18@iitg.ac.in>
2021-03-12 17:59:50 +0530

    Movie Output Styling

--- js/main.js
+++ js/main.js
@@ -6,11 +6,26 @@ function getMovies(searchText) {
     axios.get('http://www.omdbapi.com/?s='+searchText + '&apikey=9be27fce')
     .then(function (response) {
         console.log(response);
+        const movies = response.data.Search;
+        let output = '';
+
+        $.each(movies, function (index,movie){
+            output += `
+                <div class="col-mod-3">
+                    <div class="well text-center">
+                        <img src="${movie.Poster}">
+                        <h5> ${movie.Title} </h5>
+                        <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+                    </div>
+                </div>  
+            `
+        });
+
+        $('#movies').html(output);
     })
     .catch(function (err) {
         console.log(err);
     });
-
 }
 
 $(document).ready(() => {
