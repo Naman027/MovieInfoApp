dd59f947da856dbf119d357e74e0ac146628e58f
NamanGoyal07 <naman18@iitg.ac.in>
2021-03-12 22:15:26 +0530

    Movie Page

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ function getMovies(searchText) {
 
         $.each(movies, function (index,movie){
             output += `
-                <div class="col-mod-3">
+                <div class="col-md-3">
                     <div class="well text-center">
                         <img src="${movie.Poster}">
                         <h5> ${movie.Title} </h5>
@@ -26,6 +26,43 @@ function getMovies(searchText) {
     .catch(function (err) {
         console.log(err);
     });
+
+}
+
+function movieSelected(id){
+    sessionStorage.setItem('movieId',id);
+    window.location = 'movie.html';
+    return false;
+}
+
+function getMovie(){
+    const movieId = sessionStorage.getItem('movieId');
+
+    axios.get('http://www.omdbapi.com/?i='+movieId + '&apikey=9be27fce')
+    .then(function (response) {
+        console.log(response);
+        const movie  = response.data;
+
+        let output = `
+            <div class = "row">
+                <div class="col-md-4">
+                    <img src="${movie.Poster}" class="thumbnail">
+                </div>
+
+                <div class="col-md-8">
+                    <h2> ${movie.Title} </h2>
+                </div>
+
+            </div>
+
+        `;
+
+        $('#movie').html(output);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+
 }
 
 $(document).ready(() => {
