fabc061e101275c37cb47903cd9a5457493bba76
NamanGoyal07 <naman18@iitg.ac.in>
2021-03-12 17:48:05 +0530

    Getting the API from OMDB

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
+// const axios = require('axios');
+
 function getMovies(searchText) {
-    console.log(searchText);
+    // console.log(searchText);
+
+    axios.get('http://www.omdbapi.com/?s='+searchText + '&apikey=9be27fce')
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+
 }
 
 $(document).ready(() => {
