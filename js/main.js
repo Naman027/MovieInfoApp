aa9d39da15e1f8a06b6951f4c469ee73d6842900
NamanGoyal07 <naman18@iitg.ac.in>
2021-03-12 22:44:15 +0530

    Now can view Movie details

--- js/main.js
+++ js/main.js
@@ -47,14 +47,26 @@ function getMovie(){
             <div class = "row">
                 <div class="col-md-4">
                     <img src="${movie.Poster}" class="thumbnail">
+                    <h4> 
+                        ${movie.Title} </h4>
                 </div>
 
                 <div class="col-md-8">
-                    <h2> ${movie.Title} </h2>
+                    <ul class="list-group">
+                        <li class="list-group-item"><strong>Genre : </strong>${movie.Genre}</li> 
+                        <li class="list-group-item"><strong>Released : </strong>${movie.Released}</li>
+                        <li class="list-group-item"><strong>Rated : </strong>${movie.Rated}</li> 
+                        <li class="list-group-item"><strong>IMDB Rating : </strong>${movie.imdbRating} out of 10</li> 
+                        <li class="list-group-item"><strong>Directors : </strong>${movie.Director}</li> 
+                        <li class="list-group-item"><strong>Writer : </strong>${movie.Writer}</li> 
+                        <li class="list-group-item"><strong>Actors : </strong>${movie.Actors}</li>  
+                        <li class="list-group-item"><strong>Plot : </strong>${movie.Plot}</li>
+                        </ul>
+                        <hr>
+                        <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">IMDB Page</a>
+                        <a href="index.html" class="btn btn-default">Home Page</a>
                 </div>
-
             </div>
-
         `;
 
         $('#movie').html(output);
